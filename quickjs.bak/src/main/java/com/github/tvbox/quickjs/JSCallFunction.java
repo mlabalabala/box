73d5a84936625cb3d60d44fc9f87041302ed25c2
mlabalabala <satan.xyz@qq.com>
2025-09-03 23:30:49 +0800

    同步更新

--- quickjs.bak/src/main/java/com/github/tvbox/quickjs/JSCallFunction.java
+++
@@ -1,7 +0,0 @@
-package com.github.tvbox.quickjs;
-
-public interface JSCallFunction {
-
-    Object call(Object... args);
-
-}
--- quickjs.bak/src/main/java/com/whl/quickjs/wrapper/JSCallFunction.java
+++
@@ -1,7 +0,0 @@
-package com.whl.quickjs.wrapper;
-
-public interface JSCallFunction {
-
-    Object call(Object... args);
-
-}
